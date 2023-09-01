32c32ea43af155c048a71508a8d6375c14364ebe
Jeremy Rico <jeremyrico@Yudhvirs-MacBook-Air.local>
2023-09-01 16:38:00 -0700

    text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hatsune Miku is the cutest in the whole world!
+          oo aa geh
         </p>
         <a
           className="App-link"
