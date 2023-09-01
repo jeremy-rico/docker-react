c60b38d6f4aee18c34d1d5e92bdab92ca25dc5e6
Jeremy Rico <jeremyrico@Yudhvirs-MacBook-Air.local>
2023-09-01 11:49:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hatsune Miku is the cutest in the whole world!
         </p>
         <a
           className="App-link"
